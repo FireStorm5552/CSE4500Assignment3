c99904602fb13b07459043cf390a08eaf34d9d9b
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-02 21:45:24 -0800

    aaa

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/todos', function () {
+    return view('todos');
+});
+Route::get('/schedule', function () {
+    return view('schedule');
+});
+Route::get('/board', function () {
+    return view('board');
+});
 URL::forceScheme('https');
