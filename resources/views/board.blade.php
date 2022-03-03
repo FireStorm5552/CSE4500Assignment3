e706f9845643bff5faa43678ea74b9974a8345d7
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-02 22:01:31 -0800

    Update board.blade.php

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -7,7 +7,31 @@
 @stop
 
 @section('content')
-    <p>Content here</p>
+    <table id="table" class="table table-bordered">
+<div class="row">
+  <div class="card-body">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Task</th><th>Progress</th><th style="width: 40px">Label</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1.</td>
+          <td>Update software</td>
+          <td><div class="progress progress-xs"><div class="progress-bar progress-bar-danger" style="width: 55%"></div></div></td>
+          <td><span class="badge bg-danger">55%</span></td>
+        </tr>
+        <tr>
+          <td>2.</td>
+          <td>Clean database</td>
+          <td><div class="progress progress-xs"><div class="progress-bar bg-warning" style="width: 70%"></div></div></td>
+          <td><span class="badge bg-warning">70%</span></td>
+        </tr>
+      </tbody>
+  </div>
+</div>
+    </table>
 @stop
 
 @section('css')
@@ -15,5 +39,7 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
 @stop
