b97ea5a56446cd44a483f25a6c2cafdacd39acf4
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-03 16:33:49 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -41,4 +41,4 @@ Route::get('/db-migrate', function(){
 Route::get('/event-feed', function(){
 $arr = array(["title"=>"CSE4500 Class","start"=>"2022-02-23T17:30:00","end"=>"2022-02-23T18:45:00"],["title"=>"CSE4500 Class","start"=>"2022-02-28T17:30:00","end"=>"2022-02-28T18:45:00"]);
  echo json_encode($arr);
-}
+});
